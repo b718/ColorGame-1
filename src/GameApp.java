b7103cc3f8bfb979bc3fadf959f71c9f10d7ec6d
whiffy <bryanzhao168@yahoo.ca>
2021-12-27 20:20:59 -0800

    Base

---
+++ src/GameApp.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameApp {
+
+    private final Color RED = Color.RED;
+    private final Color BLUE = Color.BLUE;
+    private final Color YELLOW = Color.YELLOW;
+    private final Color GREEN = Color.GREEN;
+    public static final Font SERIF_FONT = new Font("Serif Sans", Font.PLAIN, 20);
+    public static final int WIDTH_WINDOW = 600;
+    public static final int HEIGHT_WINDOW = 600;
+
+    private JFrame window;
+
+    public GameApp(){
+
+
+    }
+}
